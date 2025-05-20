6b1f6bab4c805e4ab0c2c2e960987c7b475be747
alenatranova <alenafenderovna@gmail.com>
2025-05-20 23:11:47 +0300

    fix: change crud

--- app/crud.py
+++ app/crud.py
@@ -3,21 +3,25 @@ from . import models
 from . import schemas
 from passlib.context import CryptContext
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
 
 def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = pwd_context.hash(user.password)
+    hashed_password = get_password_hash(user.password)  # Используем новую функцию
     db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
 def get_books(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Book).offset(skip).limit(limit).all()
 
